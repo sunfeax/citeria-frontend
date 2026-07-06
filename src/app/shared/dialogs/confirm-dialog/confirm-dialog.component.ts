d79b91933c6e60f24719c5d3ea429fad5d4190d6
Vlad Uski <sunfeax@gmail.com>
2026-07-06 19:30:32 +0200

    feat(dialog): implement confirm dialog component with styles and actions

---
+++ src/app/shared/dialogs/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,25 @@
+import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
+import { Component, inject } from '@angular/core';
+import { iDialogData } from '../../types/dialog-data';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  imports: [],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss',
+})
+export class ConfirmDialogComponent {
+  /** INJECTORS */
+  private readonly dialogRefSE = inject<DialogRef<boolean>>(DialogRef);
+
+  /** DATA */
+  readonly data = inject<iDialogData>(DIALOG_DATA);
+
+  /** ACTIONS */
+  onConfirm(): void {
+    this.dialogRefSE.close(true);
+  }
+  onCancel(): void {
+    this.dialogRefSE.close(false);
+  }
+}
